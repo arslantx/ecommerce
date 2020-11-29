704abbdc870a434d03a5510e9a86317a178f1a27
Arslan Haytiyev <arslanh@gmail.com>
2020-11-29 17:29:55 -0600

    Adding password field

--- src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -5,7 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class CreateUserRequest {
 
 	@JsonProperty
-	private String username;
+    private String username;
+    
+    @JsonProperty
+    private String password;
+
+    @JsonProperty
+    private String confirmPassword;
 
 	public String getUsername() {
 		return username;
@@ -13,5 +19,21 @@ public class CreateUserRequest {
 
 	public void setUsername(String username) {
 		this.username = username;
-	}
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
